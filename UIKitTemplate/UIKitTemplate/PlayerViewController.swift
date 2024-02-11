7bf1cb864ea242ecef75f103748e916d249e4e85
Kalandarov Vakil <vak-forever@inbox.ru>
2024-02-12 01:38:47 +0300

    создал мини плеер

---
+++ UIKitTemplate/UIKitTemplate/PlayerViewController.swift
@@ -0,0 +1,18 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import AVFoundation
+import UIKit
+
+// стартовый экран приложения
+class PlayerViewController: UIViewController {
+    //MARK: - IBAction
+    @IBAction func playTrack(_ sender: Any) {
+        guard let detailVC = storyboard?.instantiateViewController(identifier: "detailVC") as? DetailViewController
+        else {
+            return
+        }
+        detailVC.createPlayer(music: "Slava Marlow - Camry 3.5")
+        present(detailVC, animated: true)
+    }
+}
